eb921b0a60c7ae050d2a97c70949bdde0e8de7dd
zuokai666 <13703391897@163.com>
2019-11-13 15:29:25 +0800

    update

--- Spring.Cloud.Node1/src/main/java/com/cloud/CloudNode1App.java
+++ Spring.Cloud.Node1/src/main/java/com/cloud/CloudNode1App.java
@@ -2,9 +2,9 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudNode1App {
 	
